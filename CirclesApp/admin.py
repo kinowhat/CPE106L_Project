dc2c1e92b572f00ac79e10e34db2c6de7ddf9468
kinowhat <shitakemush1990@gmail.com>
2026-09-25 18:52:20 +0800

    Created models for Circle and Membership, also created a superuser/admin account

--- CirclesApp/admin.py
+++ CirclesApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Circle, Membership
 
-# Register your models here.
+admin.site.register(Circle)
+admin.site.register(Membership)
--- CirclesApp/models.py
+++ CirclesApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+import secrets
+from django.contrib.auth.models import User
 # Create your models here.
+def create_code():
+    return secrets.token_urlsafe(6)
+
+class Circle(models.Model):
+    name = models.CharField(max_length=32)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    invite_code = models.CharField(max_length=12, unique=True, default=create_code() )
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    circle = models.ForeignKey(Circle, on_delete=models.CASCADE, related_name = "memberships")
+    role = models.CharField(max_length=10, default="member")
+    class Meta:
+        unique_together =("user", "circle")
+
+
